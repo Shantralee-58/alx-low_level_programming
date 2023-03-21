445ef09ef995f63e3b31c07675e5a9af52fa610e
Shantralee-58 <lindiwekhumalo833@gmail.com>
2023-03-21 04:04:15 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: 'prints _putchar to stdout'
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
