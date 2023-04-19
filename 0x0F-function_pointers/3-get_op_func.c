bacdbaef3ab1e97c7490327f6796c2bf2c4efdfd
Shantralee-58 <lindiwekhumalo833@gmail.com>
2023-04-19 15:02:58 -0700

    A program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,23 +5,25 @@
 /**
  * get_op_func - ...
  * @s: ...
+ *
  * Return: ...
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
 	};
 	int i = 0;
-	while (i > 5)
+
+	while (i < 5)
 	{
 	if (strcmp(s, ops[i].op) == 0)
-	return (ops[i].f);
+	return (ops[i].f);	
 	i++;
 	}
 	return (0);
