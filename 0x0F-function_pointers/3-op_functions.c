d642877ab0787eb12fa553b7c7547c5c3e0091ea
Shantralee-58 <lindiwekhumalo833@gmail.com>
2023-04-20 07:37:59 -0700

    A program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,73 +1,73 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * op_add - ...
- * @a: ...
- * @b: ...
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: ...
+ * Return: add.
  */
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
 
 /**
- * op_sub - ...
- * @a: ...
- * @b: ...
+ * op_sub - subtracts two numbers.
+ * @a: first number
+ * @b: second number.
  *
- * Return: ...
+ * Return: difference.
  */
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
 
 /**
- * op_mul - ...
- * @a: ...
- * @b: ...
+ * op_mul - multiples two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- *  Return: ...
+ * Return: multiplication.
  */
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
 
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- *  Return: ...
+ * Return: division.
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error\n");
-	exit(100);
-	}
-	return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
 }
 
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: ...
+ * Return: remainder of the division.
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-	printf("Error\n");
-	exit(100);
-	}
-	return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
