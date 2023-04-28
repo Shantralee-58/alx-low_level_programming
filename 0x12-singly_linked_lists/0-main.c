f170285400ecfa76389c2f503e794dd13f640686
Shantralee-58 <lindiwekhumalo833@github.com>
2023-04-28 15:47:43 -0700

    A function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -14,7 +14,6 @@ int main(void)
 	list_t *new;
 	list_t hello = {"World", 5, NULL};
 	size_t n;
-
 	head = &hello;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -28,13 +27,11 @@ int main(void)
 	head = new;
 	n = print_list(head);
 	printf("-> %lu elements\n", n);
-
 	printf("\n");
 	free(new->str);
 	new->str = NULL;
 	n = print_list(head);
 	printf("-> %lu elements\n", n);
-
 	free(new);
 	return (0);
 }
