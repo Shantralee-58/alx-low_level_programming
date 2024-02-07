953ffa2c3bb2a92556876161615a89be8f6a115b
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-02-07 11:08:36 -0800

    Write a function that searches for a value in a sorted array of integers using the Binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,35 +1,42 @@
 #include "search_algos.h"
-#include <stdio.h>
-
 /**
- * binary_search - Searches for a value in a sorted array using Binary search
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in array
- * @value: Value to search for
- * Return: Index where value is located, or -1 if not found
- */
+* binary_search - Searches for a value in a sorted array
+* of integers using binary search.
+* @array: A pointer to the first element of the array to search.
+* @size: The number of elements in the array.
+* @value: The value to search for.
+**
+Return: If the value is not present or the array is NULL, -1.
+* Otherwise, the index where the value is located.
+*/
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1, mid, i;
-
-	if (array == NULL)
-		return (-1);
-
-	while (left <= right)
-	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
-
-		mid = (left + right) / 2;
-
-		if (array[mid] == value)
-			return (mid);
-		else if (array[mid] < value)
-			left = mid + 1;
-		else
-			right = mid - 1;
-	}
-	return (-1);
+size_t i, left, right;
+/* Check if the array is NULL */
+if (array == NULL)
+return (-1);
+/* Perform binary search */
+for (left = 0, right = size - 1; right >= left;)
+{
+printf("Searching in array: ");
+for (i = left; i < right; i++)
+printf("%d, ", array[i]);
+printf("%d\n", array[i]);
+/* Calculate the middle index */
+i = left + (right - left) / 2;
+/* If the middle element is the value */
+if (array[i] == value)
+/* Return the index */
+return (i);
+/* If the middle element is greater than the value */
+if (array[i] > value)
+/* Update the right boundary */
+right = i - 1;
+else
+/* Otherwise, update the left boundary */
+left = i + 1;
+} /* Value not found, return -
+1
+*/
+return (-1);
 }
